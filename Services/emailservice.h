834fd4c37a9a3a459e754b604d0ee58417ebd7c3
Ahito498 <hassanrashwan498@gmail.com>
2025-07-08 20:07:59 +0300

    finished services classes initially

---
+++ Services/emailservice.h
@@ -0,0 +1,16 @@
+//
+// Created by Rashwan's on 08/07/2025.
+//
+
+#ifndef EMAILSERVICE_H
+#define EMAILSERVICE_H
+
+
+
+class emailservice {
+
+};
+
+
+
+#endif //EMAILSERVICE_H
---
+++ Services/shippingservice.h
@@ -0,0 +1,16 @@
+//
+// Created by Rashwan's on 08/07/2025.
+//
+
+#ifndef SHIPPINGSERVICE_H
+#define SHIPPINGSERVICE_H
+
+
+
+class shippingservice {
+
+};
+
+
+
+#endif //SHIPPINGSERVICE_H
